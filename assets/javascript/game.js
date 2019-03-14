491f696abd5d0e3ae8d5fad5c534340598e3e677
unknown <cuzvin2@gmail.com>
2019-03-14 13:20:34 -0400

    set up html structure and added images

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//There will be four crystals displayed as buttons on the page.
+
+   //The player will be shown a random number at the start of 
+   //the game.
+
+// When the player clicks on a crystal, it will add a specific
+// amount of points to the player's total score. 
